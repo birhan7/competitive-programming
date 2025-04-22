3f0a2ae7d4b23353d5a2355737068dd198711033
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-04-22 09:25:58 -0700

    Time: 4055 ms (5.16%), Space: 19.2 MB (8.16%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -0,0 +1,47 @@
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        col = row = n
+        ans = []
+        graph = defaultdict(list)
+        for u, v in redEdges:
+            graph[u].append([v,"red"])
+        for u, v in blueEdges:
+            graph[u].append([v,"blue"])
+        print(graph)
+        
+
+        def bfs(vertex):
+            queue = deque([[0,"blue"],[0,"red"]])
+            visited = [[[False,False] for _ in range(n)] for _ in range(col)]
+            step = 0
+            while queue:
+                length = len(queue)
+                for _ in range(length):
+                    curr, color = queue.popleft()
+                    if curr == vertex:
+                        return step
+                    for neighbor in graph[curr]:
+                        print("neb", neighbor)
+                        nbr_visited = False
+                        nbr_clr = neighbor[1]
+                        temp = visited[curr][neighbor[0]]
+                        if nbr_clr == "red":
+                            nbr_visited = temp[0]
+                        else:
+                            nbr_visited = temp[1]
+
+                        if color != nbr_clr and not nbr_visited:
+                            if color == "blue":
+                                temp[0] = True
+                            else:
+                                temp[1] = True
+                            queue.append(neighbor)
+                
+                step += 1
+            return -1
+
+        for i in range(n):
+            ans.append(bfs(i))
+        return ans
+
+        
