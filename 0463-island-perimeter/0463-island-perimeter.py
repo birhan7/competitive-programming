b43ae5e929275e6b89daeb8ff8b873520b539453
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-08-07 11:03:32 -0700

    Time: 265 ms (5.13%), Space: 20.6 MB (21.44%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,32 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter, row_length, col_length = 0, len(grid), len(grid[0])
+        directions = [(0,1), (0, -1), (1, 0), (-1, 0)]
+        visited = [[False for _ in range(col_length)] for _ in range(row_length)]
+
+        def inbound(row, col):
+            return 0 <= row < row_length and 0 <= col < col_length and grid[row][col] == 1
+
+        def dfs(row, col):
+            perimeter = 0
+            visited[row][col] = True
+
+            for row_change, col_change in directions:
+                new_row = row + row_change
+                new_col = col + col_change
+
+                if not inbound(new_row, new_col):
+                    perimeter += 1
+                if inbound(new_row, new_col) and not visited[new_row][new_col]:
+                        perimeter += dfs(new_row, new_col)
+            return perimeter 
+
+        for i in range(row_length):
+            for j in range(col_length):
+                if grid[i][j] == 1:
+                    return dfs(i, j)
+        
+      
+                    
+
+        
