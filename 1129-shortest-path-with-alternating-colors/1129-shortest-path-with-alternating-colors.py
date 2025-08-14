0ed3db1236256294228bce0df66b4c5426732624
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-08-14 03:31:20 -0700

    Time: 3 ms (75.42%), Space: 17.9 MB (92.42%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,45 +1,26 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        col = row = n
-        ans = []
-        graph = defaultdict(list)
-        for u, v in redEdges:
-            graph[u].append([v,"red"])
-        for u, v in blueEdges:
-            graph[u].append([v,"blue"])
-        
+        graph = [[[],[]] for _ in range(n)]
+        for a, b in redEdges:
+            graph[a][0].append(b)
+        for a, b in blueEdges:
+            graph[a][1].append(b)
 
-        def bfs(vertex):
-            queue = deque([[0,"blue"],[0,"red"]])
-            visited = [[[False,False] for _ in range(n)] for _ in range(col)]
-            step = 0
-            while queue:
-                length = len(queue)
-                for _ in range(length):
-                    curr, color = queue.popleft()
-                    if curr == vertex:
-                        return step
-                    for neighbor in graph[curr]:
-                        nbr_visited = False
-                        nbr_clr = neighbor[1]
-                        temp = visited[curr][neighbor[0]]
-                        if nbr_clr == "red":
-                            nbr_visited = temp[0]
-                        else:
-                            nbr_visited = temp[1]
+        queue = deque([(0,0),(0,1)])
+        visited = set([(0,0),(0,1)])
+        ans = [-1 for _ in range(n)]
+        length = 0
+        while queue:
+            for _ in range(len(queue)):
+                node, color = queue.popleft()
+                if ans[node] == -1:
+                    ans[node] = length
 
-                        if color != nbr_clr and not nbr_visited:
-                            if color == "blue":
-                                temp[0] = True
-                            else:
-                                temp[1] = True
-                            queue.append(neighbor)
-                
-                step += 1
-            return -1
-
-        for i in range(n):
-            ans.append(bfs(i))
+                alternative = 1 - color
+                for neighbor in graph[node][alternative]:
+                    if (neighbor,alternative) not in visited:
+                        visited.add((neighbor,alternative))
+                        queue.append((neighbor,alternative))
+            length += 1
         return ans
-
         
