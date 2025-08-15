266bed2e0a86cd5671499887fd35a6961d878cf4
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-08-15 01:04:00 -0700

    Time: 291 ms (18.21%), Space: 19.5 MB (41.53%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,37 +1,27 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
+        directions = [(-1,0),(1,0),(-1,1),(-1,-1),(1,1),(0,1),(0,-1),(1,-1)]
+        n = len(grid)
 
         def inbound(row,col):
             return 0 <= row < rows and 0 <= col < cols
 
-        def bfs(row,col):
-            nonlocal curr
-            if grid[row][col] == 1:
-                return -1
-            directions = [(-1,0),(1,0),(-1,1),(-1,-1),(1,1),(0,1),(0,-1),(1,-1)]
-            queue = deque([(row,col)])
-            visited = set((row,col))
-            distance = 0
-            curr = (row,col)
-            while queue:
-                for _ in range(len(queue)):
-                    r, c = queue.popleft()
-                    curr = (r,c)
-                    if r == rows - 1 and c  == cols - 1:
-                        return distance + 1
-                    for dr, dc in directions:
-                        new_row, new_col = r + dr, c + dc
-                        if inbound(new_row,new_col) and grid[new_row][new_col] == 0 and (new_row,new_col) not in visited:
-                            visited.add((new_row,new_col))
-                            queue.append((new_row,new_col))
-                distance += 1
-            return distance
-        curr = (0,0)
-        ans = bfs(*curr)
-        if curr != (rows - 1, cols - 1):
+        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
-        return ans
+        
+        queue = deque([(0,0,1)])
+        visited = set((0,0))
+        while queue:
+            r, c, distance = queue.popleft()
+            if r == n - 1 and c == n - 1:
+                return distance
+            for dr, dc in directions:
+                new_row, new_col = r + dr, c + dc
+                if inbound(new_row,new_col) and (new_row,new_col) not in visited and grid[new_row][new_col] == 0:
+                    visited.add((new_row,new_col))
+                    queue.append((new_row,new_col,distance + 1))
+        return -1
 
-
+        
         
