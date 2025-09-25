ac65012edb058c16c9b42bd04bfb419b06603e62
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-09-25 06:39:33 -0700

    Time: 23 ms (5.04%), Space: 23.9 MB (5.66%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dic = set(wordDict)
+        memo = {}
+        n = len(s)
+
+        def dp(l, r):
+            if r == n and s[l:r + 1] in dic:
+                return True
+            if r == n:
+                return False
+            if (l, r) not in memo:
+                include = False
+                if s[l:r+1] in dic:
+                    include = dp(l,r + 1) or dp(r + 1, r + 1)
+                exclude = dp(l,r + 1)
+                memo[(l, r)] = include or exclude
+            return memo[(l, r)]
+        return dp(0,0)
+        
