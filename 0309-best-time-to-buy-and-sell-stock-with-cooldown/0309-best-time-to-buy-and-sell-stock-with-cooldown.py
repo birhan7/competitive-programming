b689594d62d701037a02fbad901b4487aa8c07f9
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-10-17 06:40:19 -0700

    Time: 7 ms (42.66%), Space: 19 MB (30.18%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        memo = {}
+        def dfs(i, sell):
+            if i >= length:
+                return 0
+            if (i, sell) not in memo:
+                include, exclude = 0, 0
+                if not sell:
+                    include = dfs(i + 1, True) - prices[i]
+                    exclude = dfs(i + 1, sell)
+                else:
+                    include = dfs(i + 2, False) + prices[i]
+                    exclude = dfs(i + 1, sell)
+                memo[(i, sell)] = max(include, exclude)
+            return memo[(i, sell)]
+        return dfs(0, False)
+                
+        
