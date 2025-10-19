d7c0052858b8125bc5ef2695d8026e587b2a5a26
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-10-19 12:54:11 -0700

    Time: 43 ms (68.55%), Space: 31.7 MB (86.76%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -0,0 +1,19 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix = [1]
+        self.size = 0
+
+    def add(self, num: int):
+        if num == 0:
+            self.prefix = [1]
+            self.size = 0
+        else:
+            self.prefix.append(self.prefix[self.size] * num)
+            self.size += 1
+
+    def getProduct(self, k: int) -> int:
+        if k > self.size:
+            return 0
+        return self.prefix[self.size] // self.prefix[self.size - k]
+        
