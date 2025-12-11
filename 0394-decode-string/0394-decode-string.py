1821646a1fbdc65476434822402fb057d34706ea
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-12-11 19:56:57 +0300

    Time: 0 ms (100%), Space: 17.7 MB (54.39%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,44 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        n = len(s)
         stack = []
-        temp = ""
-        num = ""
-        char = ""
-        for i in range(len(s)):
-            if s[i] == "[":
-                if num:
-                    stack.append(num)
-                    num = ""
-                stack.append(0)
-            elif s[i] == "]":
-                while stack[-1] != 0:
-                    ch = stack.pop()
-                    if ch.isdigit():
-                        char = char * int(ch)
-                    else:
-                        char = ch + char
+        for r in range(n):
+            if s[r] == ']':
+                temp = ''
+                while stack[-1] != '[':
+                    temp = stack.pop() + temp
                 stack.pop()
-                if stack:
-                    ch = stack.pop()
-                    if ch.isdigit():
-                        char = char * int(ch)
-                    else:
-                        char = ch + char
-                stack.append(char)
-                char = ""
+                k = int(stack.pop())
+                base = 10
+                while stack and stack[-1].isdigit():
+                    k += (int(stack.pop()) * base)
+                    base *= 10
+                stack.append(temp * k)
             else:
-                if s[i].isdigit():
-                    if char:
-                        stack.append(char)
-                        char = ""
-                    num += s[i]
-                else:
-                    char += s[i]
-            
-        if char:
-            stack.append(char)
-        return "".join(stack)
-        
-
-
+                stack.append(s[r])
+        ans = "".join(stack)
+     
+        return ans
         
