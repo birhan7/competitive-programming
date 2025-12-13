40aaedd807002cd8b72ebf115ac5317e8e30f050
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-12-13 22:56:26 +0300

    Time: 5 ms (78.73%), Space: 17.9 MB (96.83%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.py
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -1,18 +1,12 @@
 class Solution:
     def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
-        business = set(["electronics", "grocery", "pharmacy", "restaurant"])
-        order = {"electronics":0, "grocery":1, "pharmacy":2, "restaurant":3}
-        def check(idx):
-            coupon = code[idx]
-            return (businessLine[idx] in business) and isActive[idx] and all(c.isalnum() or c == "_" for c in coupon) and coupon
+        business = {"electronics", "grocery", "pharmacy", "restaurant"}
+        def is_valid(coupon):
+            return (coupon[1].replace('_', 'a').isalnum() and (coupon[0] in business) and coupon[2])
         
-        ans = []
-        for i in range(len(code)):
-            if check(i):
-                ans.append((businessLine[i], code[i]))
-        ans.sort(key=lambda x: (order[x[0]], x[1]))
-        res = [c for _,c in ans]
-        return res
+        ans = sorted((filter(is_valid, zip(businessLine, code, isActive))))
+        return [id for _, id ,_ in ans]
+        
         
 
             
