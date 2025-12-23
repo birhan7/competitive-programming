f92159a63e96176cf84126fc30a7a3eeeec7c628
birhan7 <151516756+birhan7@users.noreply.github.com>
2025-12-23 22:37:50 +0300

    Time: 159 ms (45.31%), Space: 17.6 MB (99.12%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,14 +1,19 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        def power_of_num(num):
+        def power_of_num(x):
+            num = x
             count = 0
-            while num != 1:
-                if num % 2 == 0:
+            while True:
+                if num in memo:
+                    memo[x] = count + memo[num]
+                    break
+                elif num % 2 == 0:
                     num /= 2
                 else:
                     num = 3 * num + 1
                 count += 1
-            return count
+            return memo[x]
+        memo = {1: 0}
         nums = []
         for num in range(lo, hi + 1):
             nums.append((power_of_num(num), num))
